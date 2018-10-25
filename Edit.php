ef9cd8e63d46a78af5f23d1c4c4445f5d07580b7
Raihan Athaya Fawwaz Raihan Athaya Fawwaz <raihanathaya007.rf@gmail.com>
2018-10-25 18:54:42 +0700

    Edit - Delete

--- Edit.php
+++ Edit.php
@@ -2,22 +2,14 @@
 <html>
 <head>
 	<title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="styleform.css">
 </head>
 <body>
-	<div class="judul">
-		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
-		<h2>Menampilkan data dari database</h2>
-
-	</div>
+<a href="Index.php">Lihat Semua Data</a>
 
 	<br/>
-
-	<a href="Index.php">Lihat Semua Data</a>
-
-	<br/>
-	<h3>Input data baru</h3>
-	<form action="Input-Aksi.php" method="post">
+	<h3>Edit data</h3>
+	<form action="Edit-Aksi.php" method="post">
 		<table>
       <tr>
         <td>Id</td>
